369d5274d38e02cd141520fa49a3b5ec78147c31
Roland Gulya <mgulyaroland@gmail.com>
2024-04-12 12:49:43 +0200

    article page working with backend

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFound from "./NotFound";
+import axios from 'axios';
+import CommentList from "../components/CommentsList";
+
 const ArticlePage = () => {
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
     const { articleId } = useParams();
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+
+            const res = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = res.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+    }, []);
     const article = articles.find(x => x.name === articleId);
+
+    const addUpvote = async () => {
+        const res = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updateArticle = res.data;
+        setArticleInfo(updateArticle);
+        // setArticleInfo(res.data);
+    }
 
     if (!article) {
         return <NotFound />
@@ -11,9 +34,14 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <div className="upvotes-section">
+                <button onClick={addUpvote}>Upvote</button>
+                <p>this article has {articleInfo.upvotes} upvote(s)</p>
+            </div>
             {article.content.map((p, i) => (
                 <p key={i} >{p}</p>
             ))}
+            <CommentList comments={articleInfo.comments} />
         </>
     );
 }
