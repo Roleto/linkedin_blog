40fa41ee6a278481bbf70a9a4a8c0464b5ebdcea
Roland Gulya <mgulyaroland@gmail.com>
2024-04-17 20:45:01 +0200

    config not nul

---
+++ my-blog/src/components/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyAeuIHFZ3_h7AGTbvBhv9Bt1R78dNi26NQ",
+    authDomain: "my-react-blog-7ab08.firebaseapp.com",
+    projectId: "my-react-blog-7ab08",
+    storageBucket: "my-react-blog-7ab08.appspot.com",
+    messagingSenderId: "835863943349",
+    appId: "1:835863943349:web:9c3a4cccd553d3356286fc"
+};
+
+export default config;
